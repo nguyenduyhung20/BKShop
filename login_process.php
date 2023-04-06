1303dcc81fb925353a2b9f8ffba78aabc58296fd
DH <DH@LAPTOP-RPUH2FGN>
2023-04-06 12:45:41 +0700

    refactor login process to OOP PHP

--- login_process.php
+++ login_process.php
@@ -2,30 +2,73 @@
 session_start();
 require_once 'config.php';
 
-$username = trim($_POST['username']);
-$password = trim($_POST['password']);
+/**
+ * Sanitize input data
+ * 
+ * @param string $data
+ * @return string
+ */
+function validate_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
-if (!empty($username) && !empty($password)) {
-    try {
-        $sql = "SELECT * FROM users WHERE username = :username_email OR email = :username_email";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(":username_email", $username, PDO::PARAM_STR);
-        $stmt->execute();
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+/**
+ * User authentication functionality
+ */
+class UserAuthentication {
+    private $conn;
 
-        if ($user && password_verify($password, $user['password'])) {
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['username'] = $user['username'];
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
 
-            header("Location: dashboard.php");
-        } else {
-            echo "Invalid credentials. Please try again.";
+    /**
+     * Authenticate user given username or email and password.
+     * 
+     * @param string $username_email
+     * @param string $password
+     * @return array|null User data if successful , null if others
+     */
+    public function authenticate($username_email, $password) {
+        try {
+            $sql = "SELECT * FROM users WHERE username = :username_email OR email = :username_email";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindParam(":username_email", $username_email, PDO::PARAM_STR);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+            if ($user && password_verify($password, $user['password'])) {
+                return $user;
+            } else {
+                return null;
+            }
+        }
+        catch (PDOException $e) {
+            throw new Exception ("Error: ".$e->getMessage()) ;
         }
     }
-    catch (PDOException $e) {
-        echo "Error: ".$e->getMessage();
+}
+
+$username = validate_input($_POST['username']);
+$password = validate_input($_POST['password']);
+
+if ($username && $password) {
+    $userAuth = new UserAuthentication($conn);
+    $user = $userAuth->authenticate($username, $password);
+
+    if ($user) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+
+        header('Location: dashboard.php');
+        exit();
+    } else {
+        echo "Invalid credentials. Please try again.";
     }
 
 }
-
+$conn = null;
 ?>
