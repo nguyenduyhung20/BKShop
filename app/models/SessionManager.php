b775a7bb6f6ea87a6d50aee0778adb522768fd8f
DH <DH@LAPTOP-RPUH2FGN>
2023-04-09 16:56:02 +0700

    document session manager

--- app/models/SessionManager.php
+++ app/models/SessionManager.php
@@ -1,47 +1,92 @@
 <?php 
-
-class SessionManager{
-    
-    public function __construc() {
+final class SessionManager
+{
+    public function __construct()
+    {
         $this->startSession();
     }
 
-    public function startSession() {
-        if (session_status() !== PHP_SESSION_NONE) {
+    /**
+     * Starts a new session if one is not already active.
+     */
+    private function startSession(): void
+    {
+        if ($this->isSessionInactive()) {
             session_start();
         }
     }
 
-    public function endSession() {
-        if (session_status() !== PHP_SESSION_NONE) {
+    /**
+     * Ends the current session and clears the session data.
+     */
+    public function endSession(): void
+    {
+        if (!$this->isSessionInactive()) {
             session_destroy();
-            $_SESSION = array();
+            $_SESSION = [];
         }
     }
 
-    public function regenerateSession() {
-        if (session_status() !== PHP_SESSION_NONE) {
+    /**
+     * Regenerates the session ID to prevent session fixation attacks.
+     */
+    public function regenerateSession(): void
+    {
+        if (!$this->isSessionInactive()) {
             session_regenerate_id(true);
         }
     }
 
-    public function set($key, $value) {
-        if (session_status() !== PHP_SESSION_NONE) {
+    /**
+     * Sets a session variable.
+     *
+     * @param string $key   The key of the session variable.
+     * @param mixed  $value The value of the session variable.
+     */
+    public function set(string $key, $value): void
+    {
+        if (!$this->isSessionInactive()) {
             $_SESSION[$key] = $value;
         }
     }
 
-    public function get($key) {
-        if (session_status() !== PHP_SESSION_NONE && isset($_SESSION[$key])) {
+    /**
+     * Retrieves the value of a session variable.
+     *
+     * @param string $key The key of the session variable.
+     *
+     * @return mixed|null The value of the session variable or null if not found.
+     */
+    public function get(string $key)
+    {
+        if (!$this->isSessionInactive() && isset($_SESSION[$key])) {
             return $_SESSION[$key];
         }
+
         return null;
     }
 
-    public function isLoggedIn() {
+    /**
+     * Determines if a user is logged in based on the presence of a user ID in the session.
+     *
+     * @return bool True if the user is logged in, false otherwise.
+     */
+    public function isLoggedIn(): bool
+    {
         return $this->get('user_id') !== null;
     }
+
+    /**
+     * Checks if the session is inactive.
+     *
+     * @return bool True if the session is inactive, false otherwise.
+     */
+    private function isSessionInactive(): bool
+    {
+        return session_status() === PHP_SESSION_NONE;
+    }
 }
+
 
 
 ?>
