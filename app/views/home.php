aa539bf622907736f838994ec49242433c146fcb
DHG <duyhung8a2@gmail.com>
2023-04-24 20:44:57 +0700

    rewrite url

--- app/views/home.php
+++ app/views/home.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Trang chủ</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link type="text/css" rel="stylesheet" href="css/home.css">
-  <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <title>Trang chủ</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link type="text/css" rel="stylesheet" href="css/home.css">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
 </head>
+
 <body>
     <div class="container-fluid">
         <!-- Header -->
@@ -21,15 +29,16 @@
                 <div id="mySidenav" class="sidenav pt-5">
                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
                     <div class="dropdown">
-                        <a class="dropdown-toggle" role="button" data-toggle="dropdown" id="user_text" aria-expanded="false">
+                        <a class="dropdown-toggle" role="button" data-toggle="dropdown" id="user_text"
+                            aria-expanded="false">
                             Tài khoản
                         </a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="login.php">Đăng nhập</a>
-                            <a class="dropdown-item" href="sigin.php">Đăng ký</a>
+                            <a class="dropdown-item" href="/login">Đăng nhập</a>
+                            <a class="dropdown-item" href="/sigin">Đăng ký</a>
                         </div>
                     </div>
-                    <a href="cart.php">Giỏ hàng</a>
+                    <a href="/cart">Giỏ hàng</a>
                     <hr>
                     <a href="#">Danh mục sản phẩm</a>
                     <a href="#">Giới thiệu</a>
@@ -37,14 +46,14 @@
                     <a href="#">Thanh toán</a>
                     <a href="#">Liên hệ</a>
                 </div>
-                <button class="btn sidebar d-md-none"  onclick="openNav()" type="submit">
+                <button class="btn sidebar d-md-none" onclick="openNav()" type="submit">
                     <i class="fa fa-bars"></i>
                 </button>
                 <!-- Logo of web page -->
                 <a href="" class="px-2">
                     <img src="img/Logo.png" alt="logo" width="30" height="30" class="d-inline-block align-text-top">
                     BKShop
-                </a>            
+                </a>
             </div>
             <!-- Search bar -->
             <div class="col-md-6 search">
@@ -61,21 +70,22 @@
             <div class="col-md-2 d-none d-sm-block" style="font-size:18px;">
                 <div class="dropdown">
                     <i class="bi bi-person-circle" style="font-size:20px;"></i>
-                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" id="user_text" aria-expanded="false">
+                    <a class="dropdown-toggle" role="button" data-toggle="dropdown" id="user_text"
+                        aria-expanded="false">
                         Tài khoản
                     </a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="login.php">Đăng nhập</a>
                         <a class="dropdown-item" href="sigin.php">Đăng ký</a>
                     </div>
-                </div>   
+                </div>
             </div>
             <!-- Shopping cart -->
             <div class="col-md-2 d-none d-sm-block" style="font-size:18px;">
                 <a href="cart.php" id="shopcart_text">
                     <i class="bi bi-cart3" style="font-size:20px;"></i>
                     Giỏ hàng
-                </a>           
+                </a>
             </div>
         </div>
         <!-- Task bar -->
@@ -84,34 +94,34 @@
                 <a href="">
                     <i class="fa fa-bars"></i>
                     Danh mục sản phẩm
-                </a>            
+                </a>
             </div>
             <div class="col-md-2 col-sm-2 d-none d-sm-block intro">
-                <a href="introduction.php">
+                <a href="/introduction">
                     Giới thiệu
                 </a>
             </div>
             <div class="col-md-2 col-sm-2 d-none d-sm-block news">
-                <a href="new.php">
+                <a href="/new">
                     Tin tức
                 </a>
             </div>
             <div class="col-md-2 col-sm-2 d-none d-sm-block payment">
-                <a href="payment.php">
+                <a href="/payment">
                     Thanh toán
-                </a>           
+                </a>
             </div>
             <div class="col-md-2 col-sm-2 d-none d-sm-block contact">
-                <a href="contact.php">
+                <a href="/contact">
                     Liên hệ
-                </a>           
+                </a>
             </div>
         </div>
         <div class="row main-content">
             <div class="col-md-2 list_product">
                 <ul>
                     <li><a href="#"><i class="bi bi-laptop"></i><span>Laptop</span></a><br></li>
-                    <li><a href="list.php"><i class="bi bi-pc-display"></i><span>PC</span></a><br></li>
+                    <li><a href="/list"><i class="bi bi-pc-display"></i><span>PC</span></a><br></li>
                     <li><a href="#"><i class="bi bi-mouse"></i><span>Chuột máy tính</span></a><br></li>
                     <li><a href="#"><i class="bi bi-keyboard"></i><span>Bàn phím</span></a><br></li>
                     <li><a href="#"><i class="bi bi-headphones"></i><span>Tai nghe</span></a><br></li>
@@ -122,8 +132,8 @@
             <div class="col-md-6" style="margin-top: 10px">
                 <img src="img/slideshow_1.png" alt="pst1" width="100%" height="300px">
                 <div class="row" style="margin-top:10px">
-                    <img class = "col-md-6" src="img/slideshow_15.png" alt="pst2" width="100%" height="145px">
-                    <img class = "col-md-6" src="img/slideshow_4.png" alt="pst3" width="270px" height="145px">
+                    <img class="col-md-6" src="img/slideshow_15.png" alt="pst2" width="100%" height="145px">
+                    <img class="col-md-6" src="img/slideshow_4.png" alt="pst3" width="270px" height="145px">
                 </div>
             </div>
             <div class="col-md-3" style="margin-top: 10px">
@@ -209,7 +219,7 @@
             <div class="col-md-4">
                 <h4>HỆ THỐNG CỬA HÀNG</h4>
                 <p>- Địa chỉ 1: 268 Lý Thường Kiệt, Phường 14, Quận 10, Thành phố Hồ Chí Minh, Việt Nam</p>
-                <p>- Địa chỉ 2: Đông Hòa, Dĩ An, Bình Dương, Việt Nam</p>   
+                <p>- Địa chỉ 2: Đông Hòa, Dĩ An, Bình Dương, Việt Nam</p>
             </div>
             <div class="col-md-4">
                 <h4>THÔNG TIN VÀ CHÍNH SÁCH</h4>
@@ -221,6 +231,7 @@
             </div>
         </div>
     </footer>
-    <script src="js/home.js"></script>  
+    <script src="js/home.js"></script>
 </body>
+
 </html>
